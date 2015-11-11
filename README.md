8165f38dae2a812f036f55fb6027d3d435eec509
Maxime Ghignet <maxime.ghignet@gmail.com>
2015-11-11 20:52:40 +0100

    feat: remove JWT, we don't need it. just stringify data instead

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# jwt-flash-messages
-Use JWT to inject flash messages into your express.js pages
+# Cookie-flash-messages
+Use cookies to inject flash messages into your Express.js pages.
+Useful when you want to display a confirmation/error message when submitting a form. You usually process the form and, depending of the result, redirect the user and display a message.
+
+You can use sessions to do so but the aim was to avoid storing data on the server side.
+
+## How to use
+
+### Install
+This is a middleware to be used in Express.js.
+
+`npm install cookie-flash-messages`
+
+### Require and declare
+In your Express.js app:
+```javascript
+var express = require('express');
+var app = express();
+var cookieFlashMessages = require('cookie-flash-messages');
+
+...
+
+// Declare the middleware before your routes
+app.use(cookieFlashMessages);
+```
+
+### Use
+
+You will then be allowed to call a new method `res.flash()` in your routes:
+```javascript
+res.flash('your-message-type', 'Your message')
+res.redirect('/your-page');
+```
+The type can be anything. I personnally use `error|info|success|warning`.
+
+## Display
+
+If a flash message was set, it will be available as a `flashMessage` variable in your templates. The objects looks like this:
+```javascript
+{
+    type: 'your-message-type',
+    text: 'Your message'
+}
+```
+
+For example in a Jade template, a simple example looks like this:
+```jade
+if flashMessage
+    div(class='flash-message ' + flashMessage.type)= flashMessage.text
+```
--- index.js
+++ index.js
@@ -1,52 +1,31 @@
 'use strict';
 
-var jwt = require('jsonwebtoken');
-var expressJwt = require('express-jwt');
 var cookie = require('cookie');
 
-var JWT_SECRET = 'changeThisSecret';
+module.exports = function (req, res, next) {
 
-module.exports = {
+    // Set a flash method to the res object
+    // Usage : res.flash('error|info|success|warning', 'Your message here');
+    res.flash = function (type, text) {
 
-    flashMessageToken: expressJwt({
-        secret: JWT_SECRET,
-        requestProperty: 'flashMessage',
-        credentialsRequired: false,
-        getToken: function fromCookie(req) {
-            if (req.headers.cookie) {
-                // Parse them
-                var cookies = cookie.parse(req.headers.cookie);
-                //Will return undefined if the cookie does not exist. This is okay
-                return cookies.jwt_flash_message;
-            }
-        }
-    }),
-
-    flashMessages: function (req, res, next) {
-        // Set a flash method to the res object
-        // Usage : res.flash('error|info|success|warning', 'Your message here');
-        res.flash = function (type, message) {
-
-            var payload = {
-                type: type,
-                message: message
-            };
-            var options = {
-                expiresInMinutes: 10
-            };
-
-            var messageToken = jwt.sign(payload, JWT_SECRET, options);
-            res.cookie('jwt_flash_message', messageToken, { httpOnly: true });
+        var message = {
+            type: type,
+            text: text
         };
 
-        // Is there a flash message? If so, set it as a variable so that we can use it in the template
-        if (req.flashMessage) {
-            res.locals.flashMessage = req.flashMessage.message;
+        res.cookie('cookie_flash_message', JSON.stringify(message), { httpOnly: true });
+    };
 
-            //// Clear previous flash messages (for the next request only). It won't affect the current request
-            res.clearCookie('jwt_flash_message');
+    // Is there a flash message? If so, set it as a variable so that we can use it in the template
+    if (req.headers.cookie) {
+        var cookies = cookie.parse(req.headers.cookie);
+        if (cookies.cookie_flash_message) {
+            res.locals.flashMessage = JSON.parse(cookies.cookie_flash_message);
+
+            // Clear previous flash messages (for the next request only). It won't affect the current request
+            res.clearCookie('cookie_flash_message');
         }
-
-        next();
     }
+
+    next();
 };
